3b99a6240e8489e083d086e02b17bbb1d66631e2
Ro De Rouck <Robrecht5000@hotmail.com>
2012-06-23 20:01:52 +0200

    refactored first commit into a subclassing structure

--- procinctu.py
+++ procinctu.py
@@ -1,32 +1,32 @@
 import pyglet
 
-background = pyglet.resource.image('battlebackground.png')
+class ProcinctuWindow(pyglet.window.Window):
+    def __init__(self):
+        pyglet.window.Window.__init__(self, width = 315, height = 220) 
+        self.batch_draw = pyglet.graphics.Batch()
+        self.background = pyglet.resource.image("battlebackground.png")
+        self.image_assassin_standing = pyglet.resource.image("assassin1.png")
+        self.sprite_assassin = pyglet.sprite.Sprite(self.image_assassin_standing, x = 50, y = 30, batch = self.batch_draw) 
+        self.fps_display = pyglet.clock.ClockDisplay()
 
-image_assassin_standing = pyglet.resource.image('assassin1.png')
-sprite_assassin = pyglet.sprite.Sprite(image_assassin_standing, x = 50, y = 30) 
+    def update(self, interval):
+        # Move 10 pixels per second
+        self.sprite_assassin.x += interval * 10
 
-window = pyglet.window.Window(width=315, height=220)
+    def on_draw(self):
+        self.background.blit(0, 0)
+        self.fps_display.draw()
+        self.batch_draw.draw()
 
-def update(interval):
-    # Move 10 pixels per second
-    sprite_assassin.x += interval * 10
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.A:
+            print "The 'A' key was pressed"
+        if symbol == pyglet.window.key.RIGHT:
+            print "The 'RIGHT' key was pressed"
+            pyglet.clock.schedule_interval(func = self.update, interval = 1/60.)
+        if symbol == pyglet.window.key.LEFT:
+            print "The 'LEFT' key was pressed"
 
-@window.event
-def on_key_press(symbol, modifiers):
-    if symbol == pyglet.window.key.A:
-        print 'The \'A\' key was pressed'
-    if symbol == pyglet.window.key.RIGHT:
-        print 'The \'RIGHT\' key was pressed'
-        pyglet.clock.schedule_interval(func = update, interval = 1/60.)
-    if symbol == pyglet.window.key.LEFT:
-        print 'The \'LEFT\' key was pressed'
-
-@window.event
-def on_draw(): 
-    background.blit(0, 0)
-    sprite_assassin.draw()
-
-fps_display = pyglet.clock.ClockDisplay()
-fps_display.draw()
-
-pyglet.app.run()
+if __name__ == "__main__":
+    window = ProcinctuWindow()
+    pyglet.app.run()
