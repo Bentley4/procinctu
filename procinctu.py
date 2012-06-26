a3cbdd29c76dc6c7fbbb4e03f143c554dc67ea8c
Ro De Rouck <Robrecht5000@hotmail.com>
2012-06-26 12:29:31 +0200

    Fireball added and starting position at player position

--- procinctu.py
+++ procinctu.py
@@ -5,6 +5,13 @@ class Assassin(pyglet.sprite.Sprite):
         pyglet.sprite.Sprite.__init__(self, pyglet.resource.image("assassin1.png"), x = 50)
         self.y = 30
 
+class Fireball(pyglet.sprite.Sprite):
+    def __init__(self, batch):
+        pyglet.sprite.Sprite.__init__(self, pyglet.resource.image("fireball.png"))
+        self.x = window.player.x + 10
+        self.y = window.player.y + 10
+          
+
 class ProcinctuWindow(pyglet.window.Window):
     def __init__(self):
         pyglet.window.Window.__init__(self, width = 315, height = 220)
@@ -13,17 +20,26 @@ class ProcinctuWindow(pyglet.window.Window):
         self.player = Assassin(batch = self.batch_draw)
         self.fps_display = pyglet.clock.ClockDisplay()
         self.keys_held = []      
+        self.f_true = False
  
     def on_draw(self):
         self.background.blit(0, 0)
         self.fps_display.draw()
         self.batch_draw.draw()
         self.player.draw()
+        if self.f_true == True:
+            self.fireball.draw()
 
     def on_key_press(self, symbol, modifiers):
         self.keys_held.append(symbol)
         if symbol == pyglet.window.key.A:
+            self.fireball = Fireball(batch = self.batch_draw)
+            self.f_true = True
             print "The 'A' key was pressed"
+            pyglet.clock.schedule_interval(func = self.update3, interval =
+1/60.)         
+            
+
         if symbol == pyglet.window.key.RIGHT:
             print "The 'RIGHT' key was pressed"
             pyglet.clock.schedule_interval(func = self.update1, interval = 1/60.)
@@ -46,6 +62,10 @@ class ProcinctuWindow(pyglet.window.Window):
     def update2(self, interval): #New movement function
         if pyglet.window.key.UP in self.keys_held:
             self.player.y += 0.05
+
+    def update3(self, interval):
+        # Move 10 pixels per second
+        self.fireball.x += interval * 10
 
 if __name__ == "__main__":
     window = ProcinctuWindow()
